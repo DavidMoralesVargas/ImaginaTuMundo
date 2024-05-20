445d7006676230d208cace3c3a3b57b2fd50085a
DavidMoralesVargas <160425356+DavidMoralesVargas@users.noreply.github.com>
2024-05-19 22:19:17 -0500

    Primera parte seguridad funcional

---
+++ ImaginaTuMundo.WEB/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+
+namespace ImaginaTuMundo.WEB.AuthenticationProviders
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var oapUser = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("Nombre", "Super"),
+            new Claim("Apellido", "Admin"),
+            new Claim(ClaimTypes.Name, "dacanad@gnmail.com"),
+            new Claim(ClaimTypes.Role, "Admin")
+        },
+        authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(oapUser)));
+        }
+
+    }
+}
